5d5d612b1b20c2e064d3864e21edad9ae460ce18
taotao.lu <taotao.lu@kingstarfintech.com>
2020-10-30 12:44:12 +0800

    created minimum example

---
+++ demo-gateway/src/main/java/com/example/demogateway/DemoGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.demogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DemoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGatewayApplication.class, args);
+	}
+
+}
